d473c546f21cf767bd66e0736246a2c611dd63cf
Leo-taro FUJIMOTO <lfujimot@e1r5p1.42.fr>
2019-09-30 18:18:00 +0200

    update nacmesh

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+	
+	public Camera cam;
+	public NavMeshAgent agent;
+	public NavMeshSurface surface;
+
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			surface.BuildNavMesh();
+			if (Physics.Raycast(ray, out hit)) {
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
